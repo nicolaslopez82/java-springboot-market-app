c2a949ee6d87252b33d9bf72bd9f54bf95c1b9ca
nicolaslopez82 <nicolaslopez82@gmail.com>
2025-09-14 02:46:21 -0300

    db connection properties basic structure

---
+++ src/main/java/com/nicolaslopez82/market/Nicolaslopez82MarketApplication.java
@@ -0,0 +1,13 @@
+package com.nicolaslopez82.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nicolaslopez82MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nicolaslopez82MarketApplication.class, args);
+	}
+
+}

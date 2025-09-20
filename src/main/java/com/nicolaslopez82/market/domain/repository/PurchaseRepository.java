5edf84ac100d0554cd6abafad584e8dd4e29f383
nicolaslopez82 <nicolaslopez82@gmail.com>
2025-09-20 03:44:05 -0300

    PurchaseRepository - Domain.

---
+++ src/main/java/com/nicolaslopez82/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.nicolaslopez82.market.domain.repository;
+
+import com.nicolaslopez82.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}

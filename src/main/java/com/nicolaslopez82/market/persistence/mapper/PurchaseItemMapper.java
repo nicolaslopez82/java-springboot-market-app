796a3d5457d74e85ae1772480eeebd7f94ad618a
nicolaslopez82 <nicolaslopez82@gmail.com>
2025-09-20 04:03:43 -0300

    PurchaseItemMapper.

---
+++ src/main/java/com/nicolaslopez82/market/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,33 @@
+package com.nicolaslopez82.market.persistence.mapper;
+
+import com.nicolaslopez82.market.domain.PurchaseItem;
+import com.nicolaslopez82.market.persistence.entity.Compra;
+import com.nicolaslopez82.market.persistence.entity.ComprasProducto;
+import com.nicolaslopez82.market.persistence.entity.ComprasProductoPK;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",  uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "total", target = "totalPrice"),
+            @Mapping(source = "estado", target = "purchased")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+    //List<PurchaseItem> toPurchaseItems(List<Compra> compras);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "id.idCompra", ignore = true),
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true)
+    })
+    ComprasProducto toComprasProduct(PurchaseItem purchaseItem);
+}

0fefc544bfbc6e30dc1b8e068d0faff6257cd021
nicolaslopez82 <nicolaslopez82@gmail.com>
2025-09-25 01:54:41 -0300

    ProductController Swagger Implementation.

--- src/main/java/com/nicolaslopez82/market/web/controller/ProductController.java
+++ src/main/java/com/nicolaslopez82/market/web/controller/ProductController.java
@@ -2,6 +2,13 @@ package com.nicolaslopez82.market.web.controller;
 
 import com.nicolaslopez82.market.domain.Product;
 import com.nicolaslopez82.market.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,31 +24,76 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/all")
+    @Operation(summary = "Get all supermarket products.", description = "Get all supermarket products.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "Products not found", content = @Content)
+    })
     public ResponseEntity<List<Product>> getProduct(){
-       return new ResponseEntity(productService.findAll(), HttpStatus.OK);
+       return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+    @Operation(summary = "Get a Product by ID's Product.", description = "Get a Product by ID's Product.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "Products not found", content = @Content)
+    })
+    public ResponseEntity<Product> getProduct(@Parameter(description = "The Product's ID", required = true, example = "7")
+                                              @PathVariable("id") int productId){
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getProductsByCategoryId(@PathVariable("categoryId") int categoryId){
+    @Operation(summary = "Get a List of Product by ID's Category.", description = "Get a list of Product by ID's Category.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "Products not found", content = @Content)
+    })
+    public ResponseEntity<List<Product>> getProductsByCategoryId(@Parameter(description = "The Category ID", required = true, example = "8")
+                                                                 @PathVariable("categoryId") int categoryId){
         return productService.getProductsByCategoryId(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    @Operation(summary = "Save a Product.", description = "Save a Product in the DataBase.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "500", description = "500 Internal Server Error", content = @Content)
+    })
+    public ResponseEntity<Product> save(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Product to create", required = true,
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class),
+                    examples = @ExampleObject(value = "{ \"productName\": \"Cafe\", " +
+                                                        "\"categoryId\": \"8\", " +
+                                                        "\"price\": \"12000.0\", " +
+                                                        "\"stock\": \"1500\", " +
+                                                        "\"active\": \"true\" }")))
+            @RequestBody Product product){
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity deleteProduct(@PathVariable("id") int productId){
+    @Operation(summary = "Delete a Product.", description = "Delete a Product in the DataBase identify by Product ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "Products not found", content = @Content)
+    })
+    public ResponseEntity deleteProduct(@Parameter(description = "The Product's ID", required = true, example = "50")
+                                        @PathVariable("id") int productId){
         if(productService.delete(productId)){
             return new ResponseEntity<>(HttpStatus.OK);
         }
